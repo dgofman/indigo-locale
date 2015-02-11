e8762ab764850954b32c683a46b93467df7d73c9
David Gofman <dgofman@gmail.com>
2015-02-10 20:32:05 -0800

    Fixed dynamic module path

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ if (!indigo) {
 module.exports = {
 	init: function() {
 		var appconf = indigo.getAppConf(__dirname + '/config/app.json');
+		appconf.server.moduleDir = '/node_modules/indigolization';
+
 		indigo.app.use('/indigolization', express.static(__dirname + appconf.get('server:webdir')));
 		indigo.libs('routers').init(indigo.app, appconf, indigo.libs('reqmodel'));
 	}
